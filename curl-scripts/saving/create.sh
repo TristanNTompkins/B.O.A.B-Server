5da41ac513060a52a99bda68b7080d6c0d08a705
kailcro <kailey.crothers@uconn.edu>
2020-08-05 13:25:41 -0400

    Finished curl-scripts Create/Destroy/Index/Show/Update for income, expense, and savings

---
+++ curl-scripts/saving/create.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+API="http://localhost:4741"
+URL_PATH="/savings"
+
+curl "${API}${URL_PATH}" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Bearer ${TOKEN}" \
+  --data '{
+    "expenses": {
+      "item": "'"${ITEM}"'",
+      "amount": "'"${AMOUNT}"'"
+    }
+  }'
+
+echo
