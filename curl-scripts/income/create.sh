3c032f53ac0d9287e2da5bdfbefcd6abc914bf4e
kailcro <kailey.crothers@uconn.edu>
2020-08-05 14:15:15 -0400

    Updated create curl script data

--- curl-scripts/income/create.sh
+++ curl-scripts/income/create.sh
@@ -9,7 +9,7 @@ curl "${API}${URL_PATH}" \
   --header "Content-Type: application/json" \
   --header "Authorization: Bearer ${TOKEN}" \
   --data '{
-    "expenses": {
+    "incomes": {
       "item": "'"${ITEM}"'",
       "amount": "'"${AMOUNT}"'"
     }
--- curl-scripts/saving/create.sh
+++ curl-scripts/saving/create.sh
@@ -9,7 +9,7 @@ curl "${API}${URL_PATH}" \
   --header "Content-Type: application/json" \
   --header "Authorization: Bearer ${TOKEN}" \
   --data '{
-    "expenses": {
+    "savings": {
       "item": "'"${ITEM}"'",
       "amount": "'"${AMOUNT}"'"
     }
